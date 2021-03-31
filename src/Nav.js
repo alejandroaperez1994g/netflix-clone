d998d9832eaea58b1863e828bc995a4c158df1c1
Alejandro <alejandroaperez1994g@gmail.com>
2021-03-31 16:15:27 -0700

    adding de logo and  avatar picture for the nav bar

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import './Nav.css';
 
 function Nav() {
     return (
-        <div>
+        <div className="nav">
+            <img className="nav_logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
+                alt="Netflix Logo" />
 
+            <img className="nav_avatar"
+                src="https://ih0.redbubble.net/image.618427277.3222/flat,1000x1000,075,f.u2.jpg"
+                alt="Netflix Logo" />
         </div>
     )
 }
