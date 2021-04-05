0486c780824f6e26d9d43c57b808d785c93439b3
Alejandro <alejandroaperez1994g@gmail.com>
2021-04-05 15:19:27 -0700

    add some css, add effect at scroll down

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Nav.css';
 
+
 function Nav() {
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            } else { handleShow(false) }
+        });
+        return () => {
+            window.removeEventListener("scroll");
+        }
+    }, [])
+
     return (
-        <div className="nav">
+        <div className={`nav ${show && "nav_black"}`}>
             <img className="nav_logo"
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
                 alt="Netflix Logo" />
