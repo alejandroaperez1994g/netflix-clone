8edea5d123f09e13d1c0896e9542c2ad019c648b
Alejandro <alejandroaperez1994g@gmail.com>
2021-04-20 15:34:54 -0400

    changes

--- src/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-
-/**base requests to de movies database */
-const instance = axios.create({
-    baseURL: "https://api.themoviedb.org/3"
-});
-
-export default instance;
