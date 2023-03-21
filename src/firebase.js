88718a44523d659e7bf546c14db10b4c997b5b4f
Alejandro <alejandroaperez1994g@gmail.com>
2023-03-21 17:06:48 +0100

    feat: :recycle: refactor static api key for enviroment variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
-import firebase from "firebase";
+import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBlnmnfIo_MqH6admSIquxfiH4_Hpoic74",
-  authDomain: "netflix-clone-f37e1.firebaseapp.com",
-  projectId: "netflix-clone-f37e1",
-  storageBucket: "netflix-clone-f37e1.appspot.com",
-  messagingSenderId: "469840830421",
-  appId: "1:469840830421:web:98800de1fe7827533ca75c",
-  measurementId: "G-6MTS66L17X",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
--- src/requests.js
+++ src/requests.js
@@ -1,15 +1,14 @@
-const API_KEY = "f7182b2d73f71e8efc8c7b11f99cdb8e";
+const API_KEY = process.env.REACT_APP_MOVIE_DB_API_KEY;
 
 const requests = {
-    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=es-ES`,
-    fetchNetfilxOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213&language=es-ES`,
-    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=es-ES`,
-    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28&language=es-ES`,
-    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35&language=es-ES`,
-    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27&language=es-ES`,
-    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749&language=es-ES`,
-    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99&language=es-ES`,
-
-}
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=es-ES`,
+  fetchNetfilxOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213&language=es-ES`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=es-ES`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28&language=es-ES`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35&language=es-ES`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27&language=es-ES`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749&language=es-ES`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99&language=es-ES`,
+};
 
 export default requests;
